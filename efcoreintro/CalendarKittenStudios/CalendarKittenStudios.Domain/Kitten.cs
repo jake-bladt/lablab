fde76ceccbbf4fe64920df2592e8cf043d7faa9b
Jake Bladt <jake@jakebladt.com>
2018-02-03 11:08:44 -0500

    initial commit

---
+++ efcoreintro/CalendarKittenStudios/CalendarKittenStudios.Domain/Kitten.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarKittenStudios.Domain
+{
+    public class Kitten
+    {
+        public string Name { get; set; }
+        public DateTime Born { get; set; }
+        public String Breed { get; set; }
+    }
+}
