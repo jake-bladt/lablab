edc7eb033508278f197a2df87417b370eb6515ca
Jake Bladt <jake@jakebladt.com>
2018-02-03 11:31:37 -0500

    +context

---
+++ efcoreintro/CalendarKittenStudios/CalendarKittenStudios.Data/CKSContext.cs
@@ -0,0 +1,17 @@
+﻿using CalendarKittenStudios.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarKittenStudios.Data
+{
+    public class CKSContext: DbContext
+    {
+        public DbSet<Calendar> Calenders { get; set; }
+        public DbSet<Kitten> Kittens { get; set; }
+        public DbSet<Photographer> Photographers { get; set; }
+    }
+}
