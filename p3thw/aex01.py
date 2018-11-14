e3d7146529a0ea4e6a88acc9748acb8a163cef4b
Jake Bladt <jake@jakebladt.com>
2018-11-14 16:13:05 +0000

    +fstring experiment

--- p3thw/aex01.py
+++ p3thw/aex01.py
@@ -2,4 +2,4 @@ steps_count = 2_260_006
 days_count = 45 * 7 + 3
 average_spd = steps_count / days_count
 
-print(f"Walking {steps_count} steps in {days_count} days averages {average_spd} steps per day.")
+print(f"Walking {"{:,}".format(steps_count)} steps in {days_count} days averages {average_spd} steps per day.")
