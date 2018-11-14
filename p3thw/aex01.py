a05b59b364b2edb110db2deb681663a7625aa94f
Jake Bladt <jake@jakebladt.com>
2018-11-14 16:15:07 +0000

    +formatting

--- p3thw/aex01.py
+++ p3thw/aex01.py
@@ -1,5 +1,6 @@
 steps_count = 2_260_006
+f_steps_count = "{:,}".format(steps_count)
 days_count = 45 * 7 + 3
 average_spd = steps_count / days_count
 
-print(f"Walking {"{:,}".format(steps_count)} steps in {days_count} days averages {average_spd} steps per day.")
+print(f"Walking {f_steps_count} steps in {days_count} days averages {average_spd} steps per day.")
