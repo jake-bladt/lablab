f1f22e0da997c2a4d65204a48c1ad89d3963c681
Jake Bladt <jake@jakebladt.com>
2018-11-27 13:41:05 -0500

    +operations

--- redisclient/redisclient/Program.cs
+++ redisclient/redisclient/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+
+using ServiceStack.Redis;
+using ServiceStack.Templates;
 
 namespace redisclient
 {
@@ -6,7 +11,31 @@ namespace redisclient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (IRedisClient client = new RedisClient("ec2-52-13-28-170.us-west-2.compute.amazonaws.com"))
+            {
+                client.Password = "knotagoodpw##";
+                for(var i = 1; i <= 100; i++)
+                {
+                    client.Set($"sampleredisclient:messages:{i}", Encoding.UTF8.GetBytes("Jake was here."));
+                }
+                var sampleMessage = client.Get<String>("sampleredisclient:messages:1");
+                Console.WriteLine($"message: {sampleMessage}");
+
+                var faces = "A23456789TJQK";
+                var suits = "cdhs";
+
+                var cards = client.Lists["sampleredisclient:deck:1"];
+                cards.Clear();
+                faces.ToCharArray().ToList().ForEach((f) =>
+                {
+                    suits.ToCharArray().ToList().ForEach((s) =>
+                    {
+                        cards.Add($"{f}{s}");
+                    });
+                });
+
+                Console.ReadLine();
+            }
         }
     }
 }
