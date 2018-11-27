65da69db6cee8a3a0e7838436ff2bfaa4ce41b64
Jake Bladt <jake@jakebladt.com>
2018-11-27 13:10:00 -0500

    +redis client

---
+++ redisclient/redisclient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace redisclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
