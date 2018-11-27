211cdbf3983b273649f4eed8e254095eed2a7fc7
Jake Bladt <jake@jakebladt.com>
2018-11-27 14:15:34 -0500

    +subject type

---
+++ redisclient/redisclient/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace redisclient
+{
+    public class Subject
+    {
+        public String Name { get; set; }
+        public long ImageCount { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
