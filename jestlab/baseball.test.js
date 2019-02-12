db071b3bfbc81c49e1228a89c9545264fc783829
Jake Bladt <jake@jakebladt.com>
2019-02-11 21:20:58 -0500

    use toThrow

--- jestlab/baseball.test.js
+++ jestlab/baseball.test.js
@@ -1,12 +1,15 @@
 const baseball = require('./baseball');
 
-test('reads stats that are already present', () => {
-  const player = {
+const batter = {
     BA: 0.3,
     HR: 19
   };
 
-  expect(baseball.statisticFinder.getStatistic('BA', player)).toBeCloseTo(0.3);
-  expect(baseball.statisticFinder.getStatistic('HR', player)).toBe(19);
+test('reads stats that are already present', () => {
+  expect(baseball.statisticFinder.getStatistic('BA', batter)).toBeCloseTo(0.3);
+  expect(baseball.statisticFinder.getStatistic('HR', batter)).toBe(19);
+});
 
+test('throws an error when stats are not present', () => {
+  expect(() => baseball.statisticFinder.getStatistic('ERA', batter)).toThrow('Statistic calculator not implemented.');
 });
