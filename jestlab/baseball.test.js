12532a63cd0ddca36f88c94dab460f6ca4972d00
Jake Bladt <jake@jakebladt.com>
2019-02-11 21:01:44 -0500

    +second test

--- jestlab/baseball.test.js
+++ jestlab/baseball.test.js
@@ -2,9 +2,11 @@ const baseball = require('./baseball');
 
 test('reads stats that are already present', () => {
   const player = {
-    BA: 0.3
+    BA: 0.3,
+    HR: 19
   };
 
   expect(baseball.statisticFinder.getStatistic('BA', player)).toBe(0.3);
+  expect(baseball.statisticFinder.getStatistic('HR', player)).toBe(19);
 
 });
