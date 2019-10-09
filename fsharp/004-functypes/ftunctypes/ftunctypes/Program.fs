776b3627e0f6330a5447e8141e46e221c0cfefb1
Jake Bladt <jake@jakebladt.com>
2019-10-09 13:05:28 -0400

    overload =^..^= operator!

--- fsharp/004-functypes/ftunctypes/ftunctypes/Program.fs
+++ fsharp/004-functypes/ftunctypes/ftunctypes/Program.fs
@@ -18,6 +18,15 @@ let main argv =
     sprintf "Midfix multiplication: %d" d |> Console.WriteLine
 
     let e = (16, 0) ||> mult
-    sprintf "Double-pipe multiplication: %d" d |> Console.WriteLine
+    sprintf "Double-pipe multiplication: %d" e |> Console.WriteLine
+
+    let f = [1; 2; 3; 4; 5; 6; 7; 8] |> 
+        List.map (fun x -> x * x) |>
+        List.sum |>
+        mult 10
+    sprintf "List ops: %d" f |> Console.WriteLine
+
+    let inline (=^..^=) x y = sprintf "%d cats and %d kittens" x y
+    4 =^..^= 3 |> Console.WriteLine
 
     0
