28aaf40942b2c6d94f1c8b8c64c54bd4435942c2
Jake Bladt <jake@jakebladt.com>
2019-10-09 12:30:47 -0400

    experiment with function types

---
+++ fsharp/004-functypes/ftunctypes/ftunctypes/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let mult x y = x * y
+let add x y = x + y
+
+[<EntryPoint>]
+let main argv =
+    let a = add 2 2
+    sprintf "Simple addition: %d" a |> Console.WriteLine
+
+    let b = (+) 4 4
+    sprintf "Outfix addition: %d" b |> Console.WriteLine
+
+    let c = 8 |> add(8)
+    sprintf "Curry addition: %d" c |> Console.WriteLine
+
+    let d = 8 |> mult <| 8
+    sprintf "Midfix multiplication: %d" d |> Console.WriteLine
+
+    let e = (16, 0) ||> mult
+    sprintf "Double-pipe multiplication: %d" d |> Console.WriteLine
+
+    0
