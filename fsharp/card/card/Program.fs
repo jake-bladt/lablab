f6f7eb6a0dababf92d53e597b5017f8bc63c10ea
Jake Bladt <jake@jakebladt.com>
2019-10-23 21:46:56 -0400

    +enums and fprint

--- fsharp/card/card/Program.fs
+++ fsharp/card/card/Program.fs
@@ -1,6 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+type Suit = Clubs=1 | Diamonds=2 | Hearts=3 | Spades=4
+
+type Face = 
+    Deuce = 2  |
+    Trey  = 3  |
+    Four  = 4  |
+    Five  = 5  |
+    Six   = 6  |
+    Seven = 7  |
+    Eight = 8  |
+    Nine  = 9  |
+    Ten  = 10  |
+    Jack = 11  |
+    Queen = 12 |
+    King  = 13 |
+    Ace   = 14 
+
+type Card(face, suit) =
+    member this.Face = face
+    member this.Suit = suit
+
+    let SuitNames = [ "any", "clubs", "diamonds", "hearts", "spades" ]
+    let FaceNames = [ "any", "any", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace" ]
+
+    member this.LongName() =
+        fprint("%s of %s", FaceNames[this.Face], SuitNames[this.Suit])
 
 [<EntryPoint>]
 let main argv =
