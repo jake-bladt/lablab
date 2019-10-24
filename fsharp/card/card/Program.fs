2ce97ed4b0b2be1918f9f22488e703640df1771d
Jake Bladt <jake@jakebladt.com>
2019-10-24 08:31:54 -0400

    Explicit cast

--- fsharp/card/card/Program.fs
+++ fsharp/card/card/Program.fs
@@ -3,30 +3,30 @@
 type Suit = Clubs=1 | Diamonds=2 | Hearts=3 | Spades=4
 
 type Face = 
-    Deuce=2 |
-    Trey=3  |
-    Four  = 4  |
-    Five  = 5  |
-    Six   = 6  |
-    Seven = 7  |
-    Eight = 8  |
-    Nine  = 9  |
-    Ten  = 10  |
-    Jack = 11  |
-    Queen = 12 |
-    King  = 13 |
-    Ace   = 14 
+    Deuce=2 
+    | Trey=3 
+    | Four=4 
+    | Five=5 
+    | Six=6
+    | Seven=7
+    | Eight=8
+    | Nine=9
+    | Ten=10
+    | Jack=11
+    | Queen=12
+    | King=13 
+    | Ace=14 
 
-type Card(Face face, Suit suit) =
-    member this.Face = face
-    member this.Suit = suit
-
+type Card(face: Face, suit: Suit) =
     let SuitNames = [ "any", "clubs", "diamonds", "hearts", "spades" ]
     let FaceNames = [ "any", "any", "two", "three", "four", "five", "six",
         "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace" ]
+    
+    member this.CardFace = face
+    member this.CardSuit = suit
 
     member this.LongName() =
-        fprint("%s of %s", FaceNames[this.Face], SuitNames[this.Suit])
+        printf("%s of %s", FaceNames.[this.Face], SuitNames.[this.Suit])
 
 [<EntryPoint>]
 let main argv =
