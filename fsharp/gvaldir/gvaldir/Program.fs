764579bd06c500df3504f89e20a965d3c2a4b783
Jake Bladt <jake@jakebladt.com>
2019-10-26 10:21:47 -0400

    Create election directory validator

---
+++ fsharp/gvaldir/gvaldir/Program.fs
@@ -0,0 +1,18 @@
+﻿// Given a directory, determine if it meets the following criteria:
+// 1. It contains one or more .jpg files
+// 2. Each of those file names starts with a number > 0 and <= the number of JPEGs
+// 3. Each of those files has a string of text (called the subject name) after the number, separated by a dash -
+// 4. None of the numbers duplicate
+// 5. None of the subject names duplicate
+
+open System
+open System.IO
+
+let usage = 
+    printfn "Usage: gvaldir <election directory path>"
+
+[<EntryPoint>]
+let main argv =
+    
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
