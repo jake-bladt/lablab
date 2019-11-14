8f9162f75b8cc54a7e79ccf9ac8e1dce1ae0f2f1
Jake Bladt <jake@jakebladt.com>
2019-11-13 20:04:52 -0500

    Return JSON

---
+++ AzfLab/SandboxAAS/Playground.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SandboxAAS
+{
+    public class Playground
+    {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public Playground(string owner)
+        {
+            Name = $"{owner}'s Playground";
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public Playground()
+        {
+            Name = "Public Playground";
+            CreationDate = DateTime.UtcNow;
+        }
+    }
+}
--- AzfLab/SandboxAAS/Sandbox.cs
+++ AzfLab/SandboxAAS/Sandbox.cs
@@ -1,19 +1,23 @@
 using System;
 using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace SandboxAAS
 {
     public static class Sandbox
     {
         [FunctionName("Sandbox")]
-        public static async Task<IActionResult> Run(
+        public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
@@ -25,9 +29,10 @@ namespace SandboxAAS
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            var ret = String.IsNullOrEmpty(name) ? new Playground() : new Playground(name);
+            var json = JsonConvert.SerializeObject(ret, Formatting.Indented);
+
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(json,  Encoding.UTF8) };
         }
     }
 }
