140eb5ff19ce2c31ca43a996bf7254307bc06047
Jake Bladt <jake@jakebladt.com>
2019-12-23 23:46:13 -0500

    get step tranches

--- d3/01-movies/app.js
+++ d3/01-movies/app.js
@@ -1,6 +1,12 @@
 const parseDate = s => d3.timeParse('%Y-%m-%d')(s);
+const getTranche = i => Math.min(10000,  parseInt(i / 1000) * 1000);
+
 const type = (d) => { 
-    return  { steps: +d.Steps, date: parseDate(d.Date) } 
+    return  { 
+        steps: +d.Steps,
+        tranche: getTranche(+d.Steps),
+        date: parseDate(d.Date) 
+    } 
 };
 
 d3.csv('data/week51.csv', type).
