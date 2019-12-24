4720696c5a284cad07882224b7cb57c8fc753612
Jake Bladt <jake@jakebladt.com>
2019-12-23 23:36:11 -0500

    This is a terrible tutorial

--- d3/01-movies/app.js
+++ d3/01-movies/app.js
@@ -1,2 +1,7 @@
-d3.csv('data/week51.csv').
+const parseDate = s => d3.timeParse('%Y-%m-%d')(s);
+const type = (d) => { 
+    return  { steps: +d.Steps, date: parseDate(d.Date) } 
+};
+
+d3.csv('data/week51.csv', type).
     then(res => console.log('csv:', res));
