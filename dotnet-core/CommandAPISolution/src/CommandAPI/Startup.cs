48b4462add18b760fcc3c76945f96c2fd7d6dbcc
Jake Bladt <jake@jakebladt.com>
2020-12-14 23:43:06 -0500

    leave cstr stub

--- dotnet-core/CommandAPISolution/src/CommandAPI/Startup.cs
+++ dotnet-core/CommandAPISolution/src/CommandAPI/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Npgsql;
+
 using CommandAPI.Data;
 
 namespace CommandAPI
@@ -28,9 +30,12 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(
-                Configuration.GetConnectionString("PostgreSqlConnection"))
-            );
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, PostgreSqlCommandApiRepo>();
         }
