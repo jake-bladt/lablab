dc6e294a89de3f7462cd8e0ddb26bff786863761
Jake Bladt <jake@jakebladt.com>
2020-12-16 01:57:48 -0500

    use mocking framework

---
+++ dotnet-core/CommandAPISolution/test/CommandAPI.Tests/CommandControllerTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Controllers;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandControllerTests
+    {
+        public CommandControllerTests()
+        {
+
+        }
+
+        [Fact]
+        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
+        {
+            
+        }
+    }
+}
