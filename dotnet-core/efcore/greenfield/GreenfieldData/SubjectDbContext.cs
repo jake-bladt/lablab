643f5665ae02aa4a232eb15f01e0c7e6bf210441
Jake Bladt <jake@jakebladt.com>
2021-12-24 17:33:53 -0500

    New for 2022

---
+++ dotnet-core/efcore/greenfield/GreenfieldData/SubjectDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace GreenfieldData
+{
+    public class SubjectDbContext : DbContext 
+    {
+        public SubjectDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ dotnet-core/efcore/greenfield/greenfield/ConfigurationBuilderSingleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Microsoft.Extensions.Configuration;
+
+namespace greenfield
+{
+    public sealed class ConfigurationBuilderSingleton
+    {
+        private static ConfigurationBuilderSingleton _instance = null;
+        private static object InstanceLock = new object();
+        private static IConfigurationRoot _configuration;
+
+        private ConfigurationBuilderSingleton()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+        }
+
+        public static ConfigurationBuilderSingleton Instance
+        {
+            get
+            {
+                lock(InstanceLock)
+                {
+                    if (null == _instance) _instance = new ConfigurationBuilderSingleton();
+                }
+                return _instance;
+            }
+        }
+
+        public static IConfigurationRoot ConfigurationRoot
+        {
+            get
+            {
+                if (null == _configuration)
+                {
+                    var x = ConfigurationBuilderSingleton.Instance;
+                }
+                return _configuration;
+            }
+        }
+    }
+}
