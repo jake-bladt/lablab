1556caa61db5f1450e63b7cfbc39f4fefc701a5f
Jake Bladt <jake@jakebladt.com>
2022-01-23 18:14:49 -0500

    Happy path on next word

--- jslab/anagrams/anagrams.js
+++ jslab/anagrams/anagrams.js
@@ -21,6 +21,40 @@ function scanWord(word, found) {
   }
 }
 
+function setNextAlpha(word) {
+
+  let revOrderPos = -1;
+  let pos, firstLetter, secondLetter;
+
+  // Starting from the end of the word, find two letters that are in alphabetical order.
+  for(pos = word.length - 1; pos > 0; pos++) {
+    firstLetter = word[pos - 1];
+    secondLetter = word[pos];
+    if(firstLetter < secondLetter) {
+      revOrderPos = pos - 1;
+      break;
+    } 
+  }
+
+  // The whole word is in reverse alphabetical order.
+  if(revOrderPos === -1) return false;
+
+  // Special case: first letter needs swap
+  if(revOrderPos === 0) {
+    // break here for now while I figure out the happy path.
+    return false;
+
+  }
+
+  let before = word.substring(0, revOrderPos);
+  let after = word.substring(revOrderPos + 2);
+  
+  word = `${before}${secondLetter}${firstLetter}${after}`;
+
+  // Special case: duplicate letters?
+  return true;
+}
+
 function indexEntry(pos, word) {
   for(let i = 1; i <= word.length; i++) {
       let wordStart = word.substring(0, i);
