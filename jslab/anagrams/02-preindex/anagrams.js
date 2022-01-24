56b4ba98140fe8329a9d8fc42ff747a26f285c97
Jake Bladt <jake@jakebladt.com>
2022-01-24 02:57:53 -0500

    pre-index solution complete

--- jslab/anagrams/02-preindex/anagrams.js
+++ jslab/anagrams/02-preindex/anagrams.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log('Script loaded.');
-
 let dictionary;
 let lastWord;
 let index = {};
@@ -15,19 +13,13 @@ function indexEntry(word) {
   } else {
     index[sorted] = [word];
   }
-  console.log(`index[${sorted}] = ${index[sorted].join(", ")}`);
 }
 
 fetch(
   'https://gist.githubusercontent.com/dlants/d3b25b0f6c0bf8d023f65e86498bf9e6/raw/b310b5aff00f62f5073b3b8d366f5a639aa88ee3/3000-words.txt'
-).then(
-    (res) => {
-      console.log('Word list received.');
-      return res.text();
-    } 
+).then((res) => res.text()
 ).then(
     (text) => {
-      console.log('Word list received.');
       dictionary = (new String(text)).split('\n');
       for(let n = 0; n < dictionary.length; n++) {
         indexEntry(dictionary[n]);
@@ -36,6 +28,6 @@ fetch(
 
 function onInput(input) {
   let candidate = sortLetters(input.value);
-  const o = indexEntry[candidate] || [];
+  const o = index[candidate] || [];
   document.getElementById('output').innerHTML = JSON.stringify(o, null, 2)
 }
