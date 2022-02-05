b8d77a1ebe7fc350d2c1f570b79d55a5f4fedb95
Jake Bladt <jake@jakebladt.com>
2022-02-05 03:51:13 -0500

    Time to sleep

--- nglab/first-app/src/app/app.component.ts
+++ nglab/first-app/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'WW=^..^=D?';
   description = "What Would Tom Frost Do?";
+  hero = 'Howard T. Duck';
 }
--- nglab/first-app/src/app/hero/hero.component.ts
+++ nglab/first-app/src/app/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+
+  @Input() name: string | undefined;
 
   constructor() { }
 
